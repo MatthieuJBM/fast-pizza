0b766ebf57d108038104f7c5e3aad02fdfd304b2
Mateusz Baryla <m.baryla@protonmail.ch>
2023-08-23 20:03:08 +0200

    Flexbox added

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">XXX</div>;
+  return <div className="hidden text-sm font-semibold md:block">XXX</div>;
 }
 
 export default Username;
