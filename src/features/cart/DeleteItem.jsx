f9421fe34b09b372a2131eaeb65ca9b13e3f5b38
Mateusz Baryla <m.baryla@protonmail.ch>
2023-08-25 23:10:46 +0200

    Cart items can be deleted from cart and from menu side

---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,14 @@
+import { useDispatch } from "react-redux";
+import Button from "./../../ui/Button";
+import { deleteItem } from "./cartSlice";
+// eslint-disable-next-line react/prop-types
+function DeleteItem({ pizzaId }) {
+  const dispatch = useDispatch();
+  return (
+    <Button type="small" onClick={() => dispatch(deleteItem(pizzaId))}>
+      DELETE
+    </Button>
+  );
+}
+
+export default DeleteItem;
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/prop-types */
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
-import { useDispatch } from "react-redux";
-import { addItem } from "../cart/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
-
   const dispatch = useDispatch();
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
+
   function handleAddToCart() {
     const newItem = {
       pizzaId: id,
@@ -39,7 +42,10 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          {!soldOut && (
+
+          {isInCart && <DeleteItem pizzaId={id} />}
+
+          {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
